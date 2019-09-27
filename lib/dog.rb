462a6a3afc57ee31627afb23e282072b460b30d0
Zachary Schulz <zjschulz@gmail.com>
2019-09-27 00:11:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,21 +21,3 @@ fido.name = "Fido"
 
 snoopy = Dog.new
 snoopy.breed = "Beagle"
-
-class Person
-  def name=(person_name)
-    @name = person_name
-  end
-  
-  def name
-    @name
-  end
-  
-  def job=(person_job)
-    @job = person_job
-  end
-  
-  def job
-    @job
-  end
-end
