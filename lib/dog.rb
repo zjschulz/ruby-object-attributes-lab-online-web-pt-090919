a3c93fb03cf2445467cd48a69aca9a3fb0d54d21
Zachary Schulz <zjschulz@gmail.com>
2019-09-27 00:07:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
   def name=(dog_name)
-    @this_dog_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @this_dog_name
+    @name
   end
   
   def breed=(dog_breed)
-    @this_dog_breed = dog_breed
+    @breed = dog_breed
   end
   
   def breed
-    @this_dog_breed
+    @breed
   end
 end
 
